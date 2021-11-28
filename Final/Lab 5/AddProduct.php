cf5df26a0c8c49dfe19a06bbff00812337b51a2f
Sojib <shahrairsojib@gmail.com>
2021-11-29 03:03:40 +0600

    Lab-5

--- Final/Lab 5/AddProduct.php
+++ Final/Lab 5/AddProduct.php
@@ -1,68 +1,25 @@
-<?php
-if (isset($_POST['save'])) {
-
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "product_db";
-    $dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8";
-    $sql = "";
-
-    try {
-        $conn = new PDO($dsn, $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "INSERT INTO products (p_name, p_bp, p_sp) VALUES ('" . $_POST['name'] . "', '" . $_POST['bp'] . "', '" . $_POST['sp'] . "')";
-
-        $conn->exec($sql);
-        echo "Database created successfully<br>";
-    } catch (PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
-    }
-
-    $conn = null;
-
-    if(isset($_POST['cb']) && $_POST['cb'] == "on") {
-        header("Location: ViewProduct.php");
-        exit();
-    }
-}
-?>
-
 <!DOCTYPE html>
-<html>
-
+<html style="box-sizing: border-box;">
 <head>
-    <meta charset="utf-8">
-    <title>Lab-5</title>
+	<title>Add Products</title>
 </head>
 
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <div>
-            <fieldset>
-                <legend>ADD Product</legend>
+<h2 style="text-align: center;">Add Product </h2>
+<form action="controller/addProductControler.php" method="post" enctype="multipart/form-data">
 
-                <label for="fname">Name :</label>
-                <input type="text" id="name" name="name"><br><br>
-
-                <label for="bp">Buying Price:</label>
-                <input type="text" id="bp" name="bp"><br><br>
-
-                <label for="sp">Selling price:</label>
-                <input type="text" id="sp" name="sp"><br><br>
-
-
-                <input type="checkbox" id="cb" name="cb">
-                <label for="cb">Display</label><br>
-
-                <hr>
-
-                <input type="submit" name="save" value="Save">
-            </fieldset>
-    </form>
-    </div>
+  <div style="float: center; text-align: center;">  
+        Name: <input type="text" name="name">
+  <br><hr>
+        Buying Price: <input type="text" name="bprice">
+  <br><hr>
+        Selling Price: <input type="text" name="sprice">
+  <br><hr>
+        <input type="checkbox" name="display" value="Yes">
+        <label for="display">Display</label>
+  <br><hr>
+        <input type="submit" name="submit" value="Save"></div>
+</form>
 
 </body>
-
 </html>
--- Final/Lab 5/Editproduct.php
+++ Final/Lab 5/Editproduct.php
@@ -1,100 +1,37 @@
-<?php
-if (isset($_POST['save'])) {
+<?php if(!empty($_GET['id'])){ ?>
+<?php 
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "product_db";
-    $dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8";
-    $sql = "UPDATE  products SET p_name='".$_POST['name']."',p_bp='".$_POST['bp']."',p_sp='".$_POST['sp']." WHERE p_id='".$_POST['id']."'";
+require_once 'controller/displayControler.php';
+$tableName = 'product';
+$product = fetchProduct($tableName, $_GET['id']);
 
-    try {
-        $conn = new PDO($dsn, $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $conn->exec($sql);
-        echo "Database created successfully<br>";
-    } catch (PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
-    }
-
-    $conn = null;
-
-    if(isset($_POST['cb']) && $_POST['cb'] == "on") {
-        header("Location: ViewProduct.php");
-        exit();
-    }
-}
-elseif (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
-    header("Location: ViewProduct.php");
-    exit();
-}else{
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "product_db";
-    $dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8";
-    $sql = "";
-    $products = [];
-
-    try {
-        $conn = new PDO($dsn, $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "SELECT * FROM products where p_id = ".$_GET['id'];
-
-        $stmt = $conn->query($sql);
-        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    // echo '<pre>';
-    // var_dump($products[0]);
-    // echo '</pre>';
-    // exit();
-
-        // echo "Database created successfully<br>";
-    } catch (PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
-    }
-
-
-    $conn = null;
-
-}
-
-?>
-
+ ?>
 <!DOCTYPE html>
 <html>
-
 <head>
-    <meta charset="utf-8">
-    <title>Lab-5</title>
+	<title>Edit Product</title>
 </head>
-
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <div>
-            <fieldset>
-                <legend>EditProduct</legend>
-
-                <label for="fname">Name :</label>
-                <input type="text" id="name" name="name" value="<?php echo $products[0]['p_name'] ?>"><br><br>
-
-                <label for="bp">Buying Price:</label>
-                <input type="text" id="bp" name="bp"value="<?php echo $products[0]['p_bp'] ?>"><br><br>
-
-                <label for="sp">Selling price:</label>
-                <input type="text" id="sp" name="sp"value="<?php echo $products[0]['p_sp'] ?>"><br><br>
-                <input type="hidden" name="id"value="<?php echo $products[0]['p_id'] ?>">
-
-                <hr>
-
-                <input type="submit" name="save" value="Save">
-            </fieldset>
-    </form>
-    </div>
-
+<h2 style=" text-align: center;"> Edit Product </h2>
+<form action="controller/editProductController.php?id=<?php echo $_GET['id'] ?>" method="post" enctype="multipart/form-data">
+  <fieldset>
+<!-- <legend><B>Edit Product</B></legend> <br>
+ -->  <div style="float: left; text-align: right;">  
+  Name: <input type="text" name="name" value="<?php echo $product['Name']; ?>">
+  <br><hr>
+  Buying Price: <input type="text" name="bprice" value="<?php echo $product['BuyingPrice']; ?>">
+  <br><hr>
+  Selling Price: <input type="text" name="sprice" value="<?php echo $product['SellingPrice']; ?>">
+  <br><hr>
+  <input type="checkbox" name="display" value="Yes" <?php if($product['display']=='Yes'){echo 'checked';} ?>>
+  <label for="display">Display</label>
+  <br><hr>
+  <input type="submit" name="submit" value="Save"></div>
+</form>
+</fieldset>
 </body>
-
-</html> 
+</html>
+<?php }
+else{
+  echo "You are not allowed to visit this page";
+} ?>
